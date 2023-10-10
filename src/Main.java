d3e05f63110129538785b7d38ab73b35205ad6f7
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-10-10 11:09:30 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
-public class Main1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    static class Field extends JFrame {
+        int width = 10;
+        int height = 10;
+
+        {
+            getContentPane().setLayout(new GridLayout(height, width));
+            for (int currentX = 0; currentX < width; currentX++)
+                for (int currentY = 0; currentY < height; currentY++)
+                    add(new JButton() {{
+                        setFocusable(false);
+                    }});
+
+            //настройки окна
+            setVisible(true);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setSize(420, 430);
+
+            clear();
+        }
+
+        void clear() {
+            for (Component currentComponent : getContentPane().getComponents())
+                currentComponent.setBackground(Color.WHITE);
+        }
+    }
+
+    static Field field = new Field();
+
+    public static void main(String[] args) throws Exception {
     }
 }
